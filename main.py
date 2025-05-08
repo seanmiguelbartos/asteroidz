f85b9eb8c168dbb2e5000a7064f1383ecc16c6ab
seanmiguelbartos <sean.bartos@protonmail.com>
2025-05-08 05:45:49 -0400

    pygame logic creation, player movement defined

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 from constants import *
-
+from player import Player
 import pygame
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     black = (0, 0, 0)
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.update(dt)
         screen.fill(black)
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
     print("Starting Asteroids!")
     print(f"""Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}""")
     
